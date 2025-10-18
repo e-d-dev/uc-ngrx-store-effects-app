97771343f3a94cbea231e81b3db9d143f0d324ff
E.D. <edeibert@protonmail.com>
2025-10-18 08:23:30 +0200

    State composition with Selectors (Core Essentials)

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,11 +1,34 @@
-import { ActionReducerMap } from "@ngrx/store";
+import {
+    ActionReducerMap,
+    createSelector,
+    createFeatureSelector,
+} from "@ngrx/store";
 
 import * as fromPizzas from "./pizzas.reducer";
 
-export interface ProductState {
+export interface ProductsState {
     pizzas: fromPizzas.PizzaState;
 }
 
-export const reducers: ActionReducerMap<ProductState> = {
+export const reducers: ActionReducerMap<ProductsState> = {
     pizzas: fromPizzas.reducer,
 };
+
+export const getProductsState =
+    createFeatureSelector<ProductsState>("products");
+
+// pizzas state
+export const getPizzaState = createSelector(
+    getProductsState,
+    (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(
+    getPizzaState,
+    fromPizzas.getPizzasLoaded
+);
+export const getPizzasLoading = createSelector(
+    getPizzaState,
+    fromPizzas.getPizzasLoading
+);
